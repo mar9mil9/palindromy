cf9d9e62a17b6a118616a4bed9935e0ef7c2dce2
Mariusz Milewski <mar9mil@interia.pl>
2020-10-16 18:43:08 +0200

    word reversal

---
+++ palindromy.py
@@ -0,0 +1,13 @@
+# Moduł 4
+# Zadanie 1
+'''
+list.reverse()
+list.index(x[, start[, end]])
+
+'''
+
+def palindrom(word):
+    reversed_word = word.reverse()
+    print(reversed_word)
+    return
+
