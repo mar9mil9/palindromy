0b73f916202c743465a7ce214842919e8ef2d340
Mariusz Milewski <mar9mil@interia.pl>
2020-10-19 11:19:19 +0200

    odwracanie słowa

--- palindromy.py
+++ palindromy.py
@@ -7,8 +7,15 @@ list.index(x[, start[, end]])
 '''
 
 def palindrom(word):
-    reversed_word = word.reverse()
-    print(reversed_word)
+    #reversed_word = reversed(word)
+    #reversed_word = word.reverse()
+    #print(str(reversed_word))
+    count = 1
+    for leter in word:
+        first_to_equal = word[count - 1]
+        second_to_equal = word[-count]
+        print(f"pierwsza to {first_to_equal}, druga to {second_to_equal}")
+        count = count + 1
     return
 
-#zmiana
+print()
